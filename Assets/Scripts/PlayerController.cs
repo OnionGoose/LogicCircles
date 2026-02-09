2e2ff84a4398710e900ee1446a2c4b300701f4e4
OnionGoose <125603927+OnionGoose@users.noreply.github.com>
2026-02-08 21:47:22 -0500

    finished script and got game working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public bool right;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!right)
+        {
+            transform.Rotate(Vector3.back * 180);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DirectionalNode dn = other.GetComponent<DirectionalNode>();
+        //check direction
+        float rot = 0;
+        Vector3 dir = Vector3.zero;
+        if (dn.current == "right")
+            {
+            rot = 90;
+            dir = Vector3.back;
+        }
+        else if (dn.current == "left")
+        {
+            rot = 90;
+            dir = Vector3.forward;
+        }
+
+        transform.Rotate(dir * rot);
+        //back is right, forward is left
     }
 }
